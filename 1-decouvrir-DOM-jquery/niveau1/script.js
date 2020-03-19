c289a6d8ac3d45542fcac4fe7900f92a9afc6af9
Clement-Chalencon <clement.chalencon@le-campus-numerique.fr>
2020-03-19 13:49:16 +0100

    commit 2

--- 1-decouvrir-DOM-jquery/niveau1/script.js
+++ 1-decouvrir-DOM-jquery/niveau1/script.js
@@ -1,1 +1,32 @@
+$(document).ready(function () {
+    
+    $("#list > ul").css("background-color","red")
+    $.get("playlist.txt", function (data) {
+        salut = data.split("\n");
+        console.log(salut);
+        for (i = 0; i < salut.length; i++) {
+            $('<li>#list > ul</li>').insertAfter($('li:nth-child(2)'+salut[i]));
+            //$("#list > ul").html(salut[i]);
+        }
 
+    }, "text");
+
+
+
+
+
+    //$.get(URL,data,function(data,status,xhr),dataType)
+
+
+    // $("#list > span > button").last().click(function () {
+    //     console.log("OKEY!");
+    // });
+    // console.log("ready!");
+
+    // $("button").click(function () {
+    //     $("button").css("background-color", "red");
+    //     $("button").html("youpi");
+
+
+
+});
