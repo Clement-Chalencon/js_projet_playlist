68acb4ce241e49f08261693afbcfa51b050cf769
Clement-Chalencon <clement.chalencon@le-campus-numerique.fr>
2020-03-19 13:48:25 +0100

    1er commit

--- 1-decouvrir-DOM-jquery/niveau0/script.js
+++ 1-decouvrir-DOM-jquery/niveau0/script.js
@@ -1,1 +1,26 @@
+$(document).ready(function () {
+    console.log("ready!");
 
+    $("button").click(function () {
+        $("button").css("background-color", "red");
+        $("button").html("youpi");
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ 1-decouvrir-DOM-jquery/niveau0/script2.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $("#list > span > button").last().click(function () {
+        console.log("OKEY!");
+    });
+
+
+// console.log("ready!");
+
+// $("button").click(function () {
+//     $("button").css("background-color", "red");
+//     $("button").html("youpi");
+
+ });
